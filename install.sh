2c3fe414ec93b037eb39fd7ad6ffc650e6595374
Nick Mispoulier <nick@proclame.be>
2019-11-04 13:53:10 +0100

    Updates

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-if ! [ -f ~/.zshrc ]; then
-	ln -s ~/.dotfiles/dotfiles/.zshrc ~/.zshrc
-	source ~/.zshrc
-fi
+echo "Checking for updates & installing them. Restart if necessary"
+softwareupdate -ia --restart
 
+echo ""
+echo "Install XCode"
+xcode-select --install
+
+echo ""
+echo "Checking for Homebrew, install if missing"
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 brew update
 
 cd ~/.dotfiles/
 brew bundle
-
-## Make redis server autostart
-ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.m·l.redis.plist  # Start Redis server via launchctl
 
 ## Install Package Control & Link Sublime User Prefs Folder
 cp ~/.dotfiles/Sublime/Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/
@@ -40,3 +43,8 @@ touch ~/.hushlogin
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 mkdir ~/Documents/tmp
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true

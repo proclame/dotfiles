ca05e93ca5aff6d9d7b6933ab4c87ffd2a6cb2d5
Nick Mispoulier <nick@proclame.be>
2026-03-13 15:38:01 +0100

    Update dotfiles

--- install.sh
+++ install.sh
@@ -11,7 +11,12 @@ xcode-select --install
 echo ""
 echo "Checking for Homebrew, install if missing"
 if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Add Homebrew to PATH on Apple Silicon
+if [[ $(uname -m) == "arm64" ]]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 brew update
@@ -19,30 +24,37 @@ brew update
 cd ~/.dotfiles/
 brew bundle
 
-## Install Package Control & Link Sublime User Prefs Folder
-cp ~/.dotfiles/Sublime/Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/
-rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-ln -s ~/.dotfiles/Sublime ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+## Post-install notes
+echo "NOTE: Open 1Password > Settings > Developer and enable the SSH Agent manually."
 
+## Setup Herd - park ~/Sites and configure NVM
+mkdir -p ~/Sites
+open -a Herd
+echo ""
+echo "Herd is opening. Complete the initial setup, then press any key to continue..."
+read -r -s -k 1
 
-## @todo: BAKCBLAZE INSTALLER
+cd ~/Sites && herd park
+
+## Install default Node LTS via Herd's NVM
+export NVM_DIR="/Users/nick/Library/Application Support/Herd/config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install --lts
+nvm alias default node
+
+## Composer global packages
+echo ""
+echo "Installing Composer global packages..."
 composer global require laravel/installer
-composer global require laravel/valet
-
-## @todo: add check if exists / valet parked ?
-valet install
-mkdir ~/Sites
-cd ~/Sites
-valet park
-
 
 brew cleanup
 
 touch ~/.hushlogin
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+## Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-mkdir ~/Documents/tmp
+## Source aliases (already configured in .zshrc via source ~/.dotfiles/dotfiles/.aliases)
 
 # Specify the preferences directory
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/"
